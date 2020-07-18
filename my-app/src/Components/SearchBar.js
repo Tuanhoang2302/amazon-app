40c2a9ec7560bcc6cf2fa312fa5017ead04bbfa3
Tuanhoang2302 <55321074+Tuanhoang2302@users.noreply.github.com>
2020-07-18 21:00:54 +0700

    search bar/adobe dx

---
+++ my-app/src/Components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class SearchBar extends React.Component {
+    render(){
+        return (
+            <div>
+                Hello
+            </div>
+        );
+    }
+}
